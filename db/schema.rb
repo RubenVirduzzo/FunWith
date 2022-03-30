0a2f21b91cbe90c42688a72a8de482b5d25c1b0a
Ruben <virduzzoruben@gmail.com>
2022-03-30 14:52:05 +0200

    modified user table removing password column

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_03_30_084439) do
+ActiveRecord::Schema[7.0].define(version: 2022_03_30_124710) do
   create_table "users", force: :cascade do |t|
     t.string "fullname"
     t.string "username"
     t.integer "age"
     t.string "email"
-    t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
