9da9b50a7263a60ab97c2b72da88cf70e49db518
Ruben <virduzzoruben@gmail.com>
2022-03-30 10:45:38 +0200

    create user model

---
+++ db/migrate/20220330084439_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration[7.0]
+  def change
+    create_table :users do |t|
+      t.string :fullname
+      t.string :username
+      t.integer :age
+      t.string :email
+      t.string :password
+
+      t.timestamps
+    end
+  end
+end
