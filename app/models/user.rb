d43d8d8cbc9d66095631ee9c500f4cff944b2019
Ruben <virduzzoruben@gmail.com>
2022-03-31 09:11:27 +0200

    add age validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
     validates :fullname, presence: true, length: { maximum: 30 }
     validates :username, presence: true
+    validates :age, numericality: true
 
 end
