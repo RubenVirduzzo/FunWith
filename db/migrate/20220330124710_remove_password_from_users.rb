ba0dceb7d69f8155994b033bea453d163387269d
Ruben <virduzzoruben@gmail.com>
2022-03-31 16:47:37 +0200

    modified user table removing age column

--- db/migrate/20220330124710_remove_password_from_users.rb
+++ db/migrate/20220330124710_remove_password_from_users.rb
@@ -1,5 +1,6 @@
 class RemovePasswordFromUsers < ActiveRecord::Migration[7.0]
   def change
     remove_column :users, :password
+    remove_column :users, :age
   end
 end
---
+++ db/migrate/20220331144610_remove_age_from_users.rb
@@ -0,0 +1,4 @@
+class RemoveAgeFromUsers < ActiveRecord::Migration[7.0]
+  def change
+  end
+end
