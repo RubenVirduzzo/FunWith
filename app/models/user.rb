ca4233aeabf3cb420985af2165f7d8d0c79fc508
Ruben <virduzzoruben@gmail.com>
2022-03-31 09:11:41 +0200

    add email validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ApplicationRecord
     validates :fullname, presence: true, length: { maximum: 30 }
     validates :username, presence: true
     validates :age, numericality: true
-
+    validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
 end
