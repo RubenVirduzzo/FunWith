eac0c84b583dd37b1f89a46d5ac2b5f96a3ad4a5
Ruben <virduzzoruben@gmail.com>
2022-04-11 10:27:14 +0200

    modify styles for views

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
     validates :fullname, presence: true, length: { maximum: 30 }
     validates :username, presence: true
     validates :date_of_birth, presence: true
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  root to: "home#index"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  devise_for :users
+  root to: "home#index"
+  resource :users
 end
