64b01c1238658f665e3f89008589a1d7a4922ddf
Ruben <virduzzoruben@gmail.com>
2022-04-12 12:58:41 +0200

    generate scaffold events

---
+++ db/migrate/20220412105710_create_events.rb
@@ -0,0 +1,18 @@
+class CreateEvents < ActiveRecord::Migration[7.0]
+  def change
+    create_table :events do |t|
+      t.string :title
+      t.text :description
+      t.string :organizer
+      t.date :date_event
+      t.time :duration_time
+      t.string :place
+      t.string :min_number_of_joiners
+      t.string :max_number_of_joiners
+      t.float :price
+      t.integer :min_age
+
+      t.timestamps
+    end
+  end
+end
--- spec/models/users_spec.rb
+++ spec/models/users_spec.rb
@@ -63,10 +63,14 @@ RSpec.describe User, type: :model do
             
             let(:invalid_user_params) { valid_user_params.except(date_of_birth: Date.new(2006, 11, 26)) }
             
-            before do 
-                Time.freeze
+            before do
+                travel_to Time.new(invalid_user_params[:date_of_birth].to_i)
             end
-
+            
+            after do
+                travel_back
+            end
+            
             it 'to equal 17' do
                 user = User.new(invalid_user_params)
                 expect(user.age).to eq(17) 
@@ -76,13 +80,10 @@ RSpec.describe User, type: :model do
                 user = User.new(invalid_user_params)
                 expect(user.underage?).to be true 
             end
-
-            after do
-                Time.current
-            end
         end
 
         context "when email field has an invalid format" do
+            
             let(:invalid_user_params) { valid_user_params.except(email: "poldo.com") }
                 
             it 'data invalid' do
