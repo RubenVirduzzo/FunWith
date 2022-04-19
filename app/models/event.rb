b60aa2e2ee72ed65b9697df5c5873dc3669f91be
Ruben <virduzzoruben@gmail.com>
2022-04-19 11:52:12 +0200

    add Event's validations

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,14 @@
 class Event < ApplicationRecord
+  validates :title, presence: true, length: { maximum: 30 }
+  validates :description, presence: true
+  validates :date_event, presence: true
+  validates :duration_time, presence: true
+  validates :place, presence: true
+  validates :min_age, presence: true
 end
+
+
+# validates :fullname, presence: true, length: { maximum: 30 }
+# validates :username, presence: true
+# validates :date_of_birth, presence: true
+# validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
