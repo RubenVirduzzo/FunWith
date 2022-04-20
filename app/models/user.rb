ff90d6172a6165a2bbe3d2e6935126232ca4d59b
Ruben <virduzzoruben@gmail.com>
2022-04-20 10:22:05 +0200

    add role column to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,8 @@ class User < ApplicationRecord
   validates :date_of_birth, presence: true
   validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
 
+  enum role: [:admin, :member, :banned]
+
   def age
     ((Time.zone.now - self.date_of_birth.to_time) /  1.year.seconds).floor
   end
