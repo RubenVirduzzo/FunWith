7bad80eb7de6c7ca636cb24596cd5f96858200e9
Ruben <virduzzoruben@gmail.com>
2022-04-25 21:52:39 +0200

    modify home views

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,9 @@ Rails.application.routes.draw do
   get "/users/:id", to: "users#show"
   get "/users/ban/:id", to: "users#ban"
   get "/users/unban/:id", to: "users#unban"
+  get "/show_events", to: "home#show_events"
+  get "/show_users", to: "home#show_users"
+
 end
 
 
