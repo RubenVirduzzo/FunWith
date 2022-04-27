731aeee8d1f914c9806f00477121c0c19398291d
Ruben <virduzzoruben@gmail.com>
2022-04-27 11:19:06 +0200

    add organizer_id to events column

--- db/schema.rb
+++ db/schema.rb
@@ -1,14 +1,3 @@
-# This file is auto-generated from the current state of the database. Instead
-# of editing this file, please use the migrations feature of Active Record to
-# incrementally modify your database, and then regenerate this schema definition.
-#
-# This file is the source Rails uses to define your schema when running `bin/rails
-# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
-# be faster and is potentially less error prone than running all of your
-# migrations from scratch. Old migrations may fail to apply correctly if those
-# migrations use external dependencies or application code.
-#
-# It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2022_04_27_091119) do
   create_table "events", force: :cascade do |t|
@@ -23,6 +12,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_27_091119) do
     t.integer "min_age"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "organizer_id"
   end
 
   create_table "inscriptions", force: :cascade do |t|
@@ -32,6 +22,13 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_27_091119) do
     t.datetime "updated_at", null: false
     t.index ["event_id"], name: "index_inscriptions_on_event_id"
     t.index ["user_id"], name: "index_inscriptions_on_user_id"
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string "label"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
