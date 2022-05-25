374c22b8ad266080698aa1b5a3287572cf7036a4
Ruben <virduzzoruben@gmail.com>
2022-05-25 12:43:12 +0200

    modified views

--- config/routes.rb
+++ config/routes.rb
@@ -13,10 +13,4 @@ Rails.application.routes.draw do
   get "/users/:id", to: "users#show"
   get "/users/ban/:id", to: "users#ban"
   get "/users/unban/:id", to: "users#unban"
-  get "/show_events", to: "home#show_events"
-  get "/show_users", to: "home#show_users"
-  get "/show_subscriptions", to: "home#show_subscriptions"
-  get "/show_availables", to: "home#show_availables"
-  get "/show_tags", to: "home#show_tags"
-  get "/show_one_tag/:id", to: "home#show_one_tag"
 end
