813cf1825cbcb5735a55416cba772e45cb9e25d2
Ruben <virduzzoruben@gmail.com>
2022-07-05 10:13:10 +0200

    add set methods in controller

--- app/controllers/user_event_preferences_controller.rb
+++ app/controllers/user_event_preferences_controller.rb
@@ -1,8 +1,11 @@
 class UserEventPreferencesController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_user_event_preference, only: %i[ show edit update destroy ]
+  before_action :set_tags
+  before_action :set_user_preferences
+  before_action :set_ids
 
   def index
-    @user_event_preferences = UserEventPreference.all
   end
 
   def show
@@ -43,17 +46,34 @@ class UserEventPreferencesController < ApplicationController
     @user_event_preference.destroy
 
     respond_to do |format|
-      format.html { redirect_to user_event_preferences_url, notice: "User event preference was successfully destroyed." }
-      format.json { head :no_content }
+      format.html { redirect_to "/users/#{ @user.id }/user_event_preferences" }
     end
   end
 
   private
     def set_user_event_preference
-      @user_event_preference = UserEventPreference.find(params[:id])
+      @user_event_preference =  UserEventPreference.find(params[:id])
+    end
+
+    def set_tags
+      @tags = Tag.all
+    end
+
+    def set_user_preferences
+      @user = User.find(params[:user_id]) 
+      @user_preferences = @user.preferences
+    end
+    
+    def set_ids
+      @ids = []
+    
+      @user_preferences.each do |preference|
+        @ids << preference.tag_id 
+      end 
     end
 
     def user_event_preference_params
-      params.require(:user_event_preference).permit(:user_id, :tag_id)
+
+      params.require(:user_event_preference).permit( :user_id, :tag_id )
     end
 end
